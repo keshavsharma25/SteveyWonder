dd1718e02f8c085d8296914c93e4a1ccadfbe6fe
keshavsharma25 <skeshav0825@gmail.com>
2024-03-06 18:58:13 +0530

    feat: add Clash Grotesk Font

--- packages/nextjs/tailwind.config.js
+++ packages/nextjs/tailwind.config.js
@@ -74,6 +74,9 @@ module.exports = {
     ],
   },
   theme: {
+    fontFamily: {
+      grotesk: ["ClashGrotesk"],
+    },
     extend: {
       boxShadow: {
         center: "0 0 12px -2px rgb(0 0 0 / 0.05)",

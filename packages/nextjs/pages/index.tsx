c65da67bd4ecea6520106d587a661b757e1e3972
keshavsharma25 <skeshav0825@gmail.com>
2024-03-06 19:02:13 +0530

    refactor: make changes to header & meta header

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,22 +1,41 @@
+import Image from "next/image";
 import type { NextPage } from "next";
+import Link from "next/link";
 import { MetaHeader } from "~~/components/MetaHeader";
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <MetaHeader />
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center mb-8">
-            <span className="block text-3xl mb-10">Bring your imagination to life! Design your own</span>
-            <span className="block text-4xl font-bold mb-10">Stevey Wonder</span>
-            <span className="block text-2xl mb-2">and dress them in your favorite styles.</span>
-            <button></button>
-          </h1>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<MetaHeader />
+			<div className="flex flex-1 justify-center items-center gap-32">
+				<Image
+					src="/images/wonder.png"
+					width={500}
+					height={500}
+					alt="stevey-wonder"
+					className="rounded-lg"
+				/>
+				<div className="flex flex-col gap-4">
+					<div className="text-6xl leading-[4.375rem] font-medium">
+						<h1>Design your very own</h1>
+						<h1 className="bg-gradient-to-r from-[#8085FF] from-[-2%] via-white via-[32%] to-[#F59595] to-[67%] font-bold bg-clip-text text-transparent">
+							Stevey Wonder
+						</h1>
+					</div>
+					<div className="flex flex-col text-[#AEC1E0] font-normal text-2xl">
+						<span>Sed ut perspiciatis unde omnis iste natus error</span>
+						<span>sit voluptatem accusantium doloremque laudantium, to</span>
+					</div>
+					<Link
+						href="/steveywonder"
+						className="px-4 py-6 rounded-lg cursor-pointer bg-[#3C44FF] font-medium text-xl max-w-fit my-4"
+					>
+						Mint Stevey Wonder
+					</Link>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Home;
--- packages/nextjs/pages/steveywonder/[id].tsx
+++ packages/nextjs/pages/steveywonder/[id].tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { ShowSteveyWonder } from "~~/components/steveywonder/main/ShowSteveyWonder";
+import { useEffect } from "react";
+import { useTokenURI } from "~~/hooks/steveywonder/useTokenURI";
 
 const SteveyWonderId = () => {
-  return <div>SteveyWonderId</div>;
+	const { tokenURI } = useTokenURI({
+		contractName: "SteveyWonder",
+		tokenId: BigInt(1),
+	});
+
+	useEffect(() => {
+		console.log(tokenURI);
+	}, [tokenURI]);
+
+	return (
+		<section className="flex flex-1 justify-center">
+			<div className="flex">
+				<ShowSteveyWonder
+					data={`data:image/svg+xml;base64,${tokenURI}`}
+					width={518}
+					height={607}
+				/>
+			</div>
+		</section>
+	);
 };
 
 export default SteveyWonderId;

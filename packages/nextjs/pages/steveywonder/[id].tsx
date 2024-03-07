1afb250444752c457c76d7de30b635286d498103
keshavsharma25 <skeshav0825@gmail.com>
2024-03-07 21:40:31 +0530

    feat: create steveywonder/[id] ui

--- packages/nextjs/pages/steveywonder/[id].tsx
+++ packages/nextjs/pages/steveywonder/[id].tsx
@@ -1,28 +1,23 @@
+import { ItemContainer } from "~~/components/steveywonder";
 import { ShowSteveyWonder } from "~~/components/steveywonder/main/ShowSteveyWonder";
-import { useEffect } from "react";
 import { useTokenURI } from "~~/hooks/steveywonder/useTokenURI";
 
 const SteveyWonderId = () => {
-	const { tokenURI } = useTokenURI({
-		contractName: "SteveyWonder",
-		tokenId: BigInt(1),
-	});
+  const { tokenURI } = useTokenURI({
+    contractName: "SteveyWonder",
+    tokenId: BigInt(1),
+  });
 
-	useEffect(() => {
-		console.log(tokenURI);
-	}, [tokenURI]);
-
-	return (
-		<section className="flex flex-1 justify-center">
-			<div className="flex">
-				<ShowSteveyWonder
-					data={`data:image/svg+xml;base64,${tokenURI}`}
-					width={518}
-					height={607}
-				/>
-			</div>
-		</section>
-	);
+  return (
+    <section className="flex flex-1 justify-center items-center gap-12">
+      <div className="flex gap-12">
+        <ShowSteveyWonder data={tokenURI} width={518} height={607} />
+        <div className={"bg-[#161728] w-[44rem] p-10 rounded-2xl shadow-md"}>
+          <ItemContainer />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SteveyWonderId;

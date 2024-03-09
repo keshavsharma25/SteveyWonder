6e4b73a7946d5d239dd1e335354644ca9d83ae64
keshavsharma25 <skeshav0825@gmail.com>
2024-03-09 17:18:44 +0530

    feat: get id:number from serverside for steveywonder/[id]

--- packages/nextjs/pages/steveywonder/[id].tsx
+++ packages/nextjs/pages/steveywonder/[id].tsx
@@ -1,23 +1,40 @@
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import { ItemContainer } from "~~/components/steveywonder";
 import { ShowSteveyWonder } from "~~/components/steveywonder/main/ShowSteveyWonder";
 import { useTokenURI } from "~~/hooks/steveywonder/useTokenURI";
 
-const SteveyWonderId = () => {
+const SteveyWonderId = ({ id }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const { tokenURI } = useTokenURI({
     contractName: "SteveyWonder",
-    tokenId: BigInt(1),
+    tokenId: BigInt(id),
   });
 
   return (
     <section className="flex flex-1 justify-center items-center gap-12">
-      <div className="flex gap-12">
-        <ShowSteveyWonder data={tokenURI} width={518} height={607} />
-        <div className={"bg-[#161728] w-[44rem] p-10 rounded-2xl shadow-md"}>
-          <ItemContainer />
+      {id < 1 ? (
+        <div className="flex justify-center items-center">
+          <h1>Invalid Stevey Wonder</h1>
         </div>
-      </div>
+      ) : (
+        <div className="flex gap-12">
+          <ShowSteveyWonder data={tokenURI} width={518} height={607} />
+          <div className={"bg-[#161728] w-[44rem] p-10 rounded-2xl shadow-md"}>
+            <ItemContainer tokenId={Number(id)} />
+          </div>
+        </div>
+      )}
     </section>
   );
+};
+
+export const getServerSideProps = (context: GetServerSidePropsContext) => {
+  const id = context.query.id;
+
+  if (typeof id === "string") {
+    return { props: { id: Number(id) } };
+  }
+
+  return { props: { id: -1 } };
 };
 
 export default SteveyWonderId;

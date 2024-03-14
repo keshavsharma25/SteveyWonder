ae0b4b086d3e1112b779315405cfe953b106df72
keshavsharma25 <skeshav0825@gmail.com>
2024-03-15 01:17:29 +0530

    feat: add toast on success, loading & other ui changes

--- packages/nextjs/components/accessories/MintAccs.tsx
+++ packages/nextjs/components/accessories/MintAccs.tsx
@@ -1,19 +1,30 @@
+import toast from "react-hot-toast";
 import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+import { cn } from "~~/utils/cn";
 import { ContractName } from "~~/utils/scaffold-eth/contract";
 
 type Props = {
   name: string;
   contractName: Exclude<ContractName, "SteveyWonder" | "Account" | "ERC6551Registry">;
-  tokenId: bigint;
+  tokenId: number;
 };
 
 export const MintAccs = ({ name, contractName, tokenId }: Props) => {
   // const { address } = useAccount();
 
-  const { write } = useScaffoldContractWrite({
+  const { write, isLoading } = useScaffoldContractWrite({
     contractName: contractName,
     functionName: "safeMint",
-    args: [tokenId],
+    args: [BigInt(tokenId)],
+    onSuccess: () => {
+      toast("Success! You can now see your new accessory in your wallet.", {
+        icon: "👏",
+        style: {
+          background: "#5AB562",
+          color: "white",
+        },
+      });
+    },
   });
 
   return (
@@ -21,9 +32,11 @@ export const MintAccs = ({ name, contractName, tokenId }: Props) => {
       onClick={() => {
         write?.();
       }}
-      className="bg-[#3C44FF] hover:bg-[#3036cc] text-white font-bold py-2 px-4 rounded"
+      className={cn(
+        "flex items-center text-xl bg-[#3C44FF] hover:bg-[#3036cc] text-white font-medium py-2 px-4 rounded",
+      )}
     >
-      {name}
+      {isLoading ? <span className="loading loading-spinner loading-md"></span> : <>{name}</>}
     </button>
   );
 };

951db59de74afd2b76c1f95b37bad9c430099055
keshavsharma25 <skeshav0825@gmail.com>
2024-03-15 01:19:47 +0530

    fix: add loading bars when fetching tokenURI

--- packages/nextjs/components/steveywonder/common/ShowTokenURI.tsx
+++ packages/nextjs/components/steveywonder/common/ShowTokenURI.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { useTokenURI } from "~~/hooks/steveywonder/useTokenURI";
 import { ContractName } from "~~/utils/scaffold-eth/contract";
@@ -10,13 +11,23 @@ type Props = {
 };
 
 export const ShowTokenURI = ({ contractName, tokenId, width, height }: Props) => {
-  const { tokenURI, isLoading } = useTokenURI({
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const { tokenURI } = useTokenURI({
     contractName,
     tokenId,
   });
 
+  useEffect(() => {
+    if (!tokenURI) {
+      setIsLoading(true);
+      return;
+    }
+    setIsLoading(false);
+  }, [tokenURI]);
+
   return isLoading ? (
-    <span className="loading loading-bars loading-lg"></span>
+    <div className="bg-[#161728] animate-pulse w-[21.25rem] h-[21.25rem]"></div>
   ) : (
     <Image src={`data:image/svg+xml;base64,${tokenURI}`} alt="SteveyWonder Token" width={width} height={height} />
   );

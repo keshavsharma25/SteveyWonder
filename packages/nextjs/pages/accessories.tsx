9c462d14e643cd38b6f13b5c03ac07f2e561be99
keshavsharma25 <skeshav0825@gmail.com>
2024-03-15 01:35:27 +0530

    feat: update Accessory Page
    - add item cards
    - show Toast when stevey wonder is not minted for a certain addr.
    - responsive
    - TokenOfOwnerByIndex: useScaffoldContractRead onSuccess & onError for updating setAccessoryTokenOption

--- packages/nextjs/pages/accessories.tsx
+++ packages/nextjs/pages/accessories.tsx
@@ -1,10 +1,15 @@
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { NextPage } from "next";
+import toast from "react-hot-toast";
+import { useAccount } from "wagmi";
 import { MintAccs } from "~~/components/accessories/MintAccs";
+import { TokenOption } from "~~/components/accessories/TokenOption";
+import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
 import { ContractName } from "~~/utils/scaffold-eth/contract";
 
-type Accessory = {
-  title: Exclude<ContractName, "SteveyWonder" | "Account" | "ERC6551Registry">;
+export type Accessory = {
+  title: Exclude<ContractName, "SteveyWonder" | "Account" | "ERC6551Registry" | "Background">;
   imagePath: string;
 };
 
@@ -31,22 +36,122 @@ const accessories: Accessory[] = [
   },
 ];
 
+type AccTokenOption = {
+  [key in Exclude<ContractName, "SteveyWonder" | "Account" | "ERC6551Registry" | "Background">]: number;
+};
+
 const Accessories: NextPage = () => {
+  const { address } = useAccount();
+
+  const { data: balanceOfStevey } = useScaffoldContractRead({
+    contractName: "SteveyWonder",
+    functionName: "balanceOf",
+    args: [address],
+  });
+
+  const { data: tokenOfOwnerByIndex, error } = useScaffoldContractRead({
+    contractName: "SteveyWonder",
+    functionName: "tokenOfOwnerByIndex",
+    args: [address, BigInt(0)],
+    onSuccess: data => {
+      accessories.forEach(({ title }) => {
+        setAccTokenOption(prev => {
+          return {
+            ...prev,
+            [title]: Number(data),
+          };
+        });
+      });
+    },
+    onError: () => {
+      accessories.forEach(({ title }) => {
+        setAccTokenOption(prev => {
+          return {
+            ...prev,
+            [title]: -1,
+          };
+        });
+      });
+    },
+  });
+
+  const AccTokenOptionState: AccTokenOption = accessories.reduce(
+    (acc, { title }) => ({
+      ...acc,
+      [title]: error ? -1 : Number(tokenOfOwnerByIndex),
+    }),
+    {} as AccTokenOption,
+  );
+
+  const [accTokenOption, setAccTokenOption] = useState<AccTokenOption>(AccTokenOptionState);
+
+  useEffect(() => {
+    console.log(accTokenOption);
+  }, [accTokenOption]);
+
   return (
     <div className="mx-[5rem] my-[4.5rem]">
       <h1 className="font-bold text-4xl pb-8">Accessories</h1>
-      <div className="grid grid-cols-4 gap-y-16">
+      <div className="w-fit flex flex-row gap-10 flex-wrap">
         {accessories.map(({ title, imagePath }) => (
-          <div
-            key={title}
-            className="max-w-[20rem] max-h-[23rem] rounded-xl bg-[#161829] border border-gray-100 border-opacity-5"
-          >
-            <div className="relative mx-3 mt-3">
+          <div key={title} className="py-4 px-2 rounded-xl bg-[#161829] border border-gray-100 border-opacity-5">
+            <div className="flex justify-center">
               <Image src={`/images/accessories/${imagePath}`} alt={title} width={300} height={300} />
             </div>
-            <div className="flex flex-row justify-between mx-8 my-5">
-              <div className="text-2xl font-semibold">{title}</div>
-              <MintAccs contractName={title} name="Mint" tokenId={BigInt(1)} key={title} />
+            <div className="flex flex-col gap-1 mb-2">
+              <div className="text-2xl font-medium mx-6 mt-4">{title}</div>
+              <hr className="mx-5 h-0.5 border-gray-500 my-2 opacity-10" />
+              <div className="flex flex-col gap-2 md:mx-5">
+                <div className="flex justify-center md:justify-normal">
+                  <span className="text-xl font-medium text-gray-300">For Stevey</span>
+                </div>
+                <div className="flex flex-col gap-2 md:flex-row md:justify-between items-center">
+                  {balanceOfStevey !== undefined ? (
+                    <select
+                      className="select select-md select-bordered min-w-[50%] rounded-xl text-center p-0"
+                      onChange={e =>
+                        setAccTokenOption(prev => {
+                          return {
+                            ...prev,
+                            [title]: Number(e.target.value),
+                          };
+                        })
+                      }
+                    >
+                      {Number(balanceOfStevey) > 0 ? (
+                        [...Array(Number(balanceOfStevey))].map((_, index) => {
+                          return <TokenOption key={index} index={BigInt(index)} />;
+                        })
+                      ) : (
+                        <option disabled selected>
+                          0
+                        </option>
+                      )}
+                    </select>
+                  ) : (
+                    <div className="loading loading-spinner text-secondary-content"></div>
+                  )}
+                  {accTokenOption[title] > 0 ? (
+                    <MintAccs contractName={title} name="Mint" key={title} tokenId={accTokenOption[title]} />
+                  ) : (
+                    <button
+                      className="flex items-center text-xl bg-[#3C44FF] hover:bg-[#3036cc] text-white font-medium py-2 px-4 rounded"
+                      onClick={() => {
+                        toast("You need to mint a Stevey first!", {
+                          icon: "🦊",
+                          style: {
+                            borderRadius: "10px",
+                            background: "#3C44FF",
+                            color: "#fff",
+                          },
+                        });
+                      }}
+                    >
+                      Mint
+                    </button>
+                  )}
+                </div>
+              </div>
             </div>
           </div>
         ))}

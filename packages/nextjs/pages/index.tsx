bdf200c944413c1e04d87eae07c9e544db7bd8ac
keshavsharma25 <skeshav0825@gmail.com>
2024-03-15 01:36:36 +0530

    fix: css changes for dark mode, etc.

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,18 +1,47 @@
+import { useLayoutEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import type { NextPage } from "next";
+import { useDarkMode } from "usehooks-ts";
 import { MetaHeader } from "~~/components/MetaHeader";
+import { cn } from "~~/utils/cn";
 
 const Home: NextPage = () => {
+  const [isDark, setIsDark] = useState(false);
+  const { isDarkMode } = useDarkMode();
+
+  useLayoutEffect(() => {
+    setIsDark(isDarkMode);
+  }, [isDarkMode]);
+
   return (
     <>
       <MetaHeader />
       <div className="flex flex-1 justify-center items-center gap-32">
-        <Image src="/images/wonder.png" width={500} height={500} alt="stevey-wonder" />
+        <div className="">
+          <Image
+            src="/images/wonder.png"
+            width={500}
+            height={700}
+            style={{
+              height: "auto",
+              width: "auto",
+            }}
+            alt="stevey-wonder"
+            priority={true}
+            quality={100}
+          />
+        </div>
         <div className="flex flex-col gap-4">
           <div className="text-6xl leading-[4.375rem] font-medium">
             <h1>Design your very own</h1>
-            <h1 className="bg-gradient-to-r from-[#8085FF] from-[-2%] via-white via-[32%] to-[#F59595] to-[67%] font-bold bg-clip-text text-transparent">
+            <h1
+              className={cn(
+                "bg-gradient-to-r from-[#8085FF] from-[-2%] via-[32%] via-white",
+                !isDark && "via-[#C7D6EC]",
+                "to-[#F59595] to-[67%] font-bold bg-clip-text text-transparent",
+              )}
+            >
               Stevey Wonder
             </h1>
           </div>
@@ -22,7 +51,7 @@ const Home: NextPage = () => {
           </div>
           <Link
             href="/steveywonder"
-            className="px-4 py-6 rounded-lg cursor-pointer bg-[#3C44FF] font-medium text-xl max-w-fit my-4"
+            className="px-4 py-6 rounded-lg cursor-pointer bg-[#3C44FF] font-medium text-xl text-white max-w-fit my-4"
           >
             Mint Stevey Wonder
           </Link>

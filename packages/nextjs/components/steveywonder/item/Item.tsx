367c3b35c3c3cc458f836cf08d05e31aca511e1e
keshavsharma25 <skeshav0825@gmail.com>
2024-03-18 03:59:00 +0530

    refactor: ui changes

--- packages/nextjs/components/steveywonder/item/Item.tsx
+++ packages/nextjs/components/steveywonder/item/Item.tsx
@@ -6,6 +6,7 @@ import { useCurrentAccessory } from "~~/hooks/steveywonder/useCurrentAccessory";
 import { useSetAccessory } from "~~/hooks/steveywonder/useSetAccessory";
 import { useTokenURI } from "~~/hooks/steveywonder/useTokenURI";
 import { Option } from "~~/types/steveywonder";
+import { cn } from "~~/utils/cn";
 
 type Props = {
   idx: bigint;
@@ -53,26 +54,31 @@ export const Item = ({ idx, option, tba, mainTokenId }: Props) => {
   }, [tokenURI]);
 
   return (
-    <div className="flex flex-col gap-1 w-[12rem] rounded-t-lg bg-primary p-2">
+    <div
+      className={cn(
+        "flex flex-col gap-1 w-[12rem] rounded-xl border border-gray-700 bg-[#1D1E35]",
+        activeTokenId === tokenId && "border-[2px] border-white bg-[#242642]",
+      )}
+    >
       <div className="overflow-hidden rounded h-full w-full">
         {isLoading ? (
-          <div className="bg-primary animate-pulse h-full w-full"></div>
+          <div className="bg-[#1D1E35] animate-pulse h-full w-full"></div>
         ) : (
           <Image src={`data:image/svg+xml;base64,${tokenURI}`} alt={`${Number(tokenId)}`} width={420} height={420} />
         )}
       </div>
       {activeStatus === "loading" ? (
         <button
-          className="flex justify-center items-center bg-secondary border border-gray-800 text-white p-2 rounded-lg cursor-not-allowed"
+          className="flex justify-center items-center border border-gray-600 text-white rounded-xl p-2 cursor-not-allowed"
           disabled={activeStatus === "loading"}
         >
           <span className="loading loading-spinner"></span>
         </button>
       ) : activeTokenId === tokenId ? (
-        <div className="text-center">Current</div>
+        <div className="flex justify-center items-center text-center font-medium p-2 m-1">Current</div>
       ) : (
         <button
-          className="bg-secondary border border-gray-800 text-white p-2 rounded-lg"
+          className="border border-gray-500 bg-white bg-opacity-5 p-2 m-1 rounded-xl"
           onClick={async () => {
             await writeAsync();
           }}
